ac0634019d87a2cc48a27424d76aa10026f4c845
Amit Sharma <161241599+Amit292004@users.noreply.github.com>
2026-09-26 12:41:29 +0530

    feat: update LeetCode 205. Isomorphic Strings solution (java)

--- problems/isomorphic-strings/solution.java
+++ problems/isomorphic-strings/solution.java
@@ -1,32 +1,33 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        char[]a=new char[128];//'\0'
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            char dh=t.charAt(i);
+            int idx=(int)ch;
+            if(a[idx]=='\0') a[idx]=dh;
+            else{
+                if(a[idx]!=dh)return false;
+            }
+            
 
-        if (s.length() != t.length()) {
-            return false;
         }
 
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Character> map2 = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-
-            // s -> t mapping
-            if (map1.containsKey(a) && map1.get(a) != b) {
-                return false;
+        char[]b =new char[128];//'\0'
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            char dh=s.charAt(i);
+            int idx=(int)ch;
+            if(b[idx]=='\0') b[idx]=dh;
+            else{
+                if(b[idx]!=dh)return false;
             }
 
-            // t -> s mapping
-            if (map2.containsKey(b) && map2.get(b) != a) {
-                return false;
-            }
 
-            map1.put(a, b);
-            map2.put(b, a);
         }
-
         return true;
+
+    
     }
 }
